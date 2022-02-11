76b9b7be05f56f5cc21dadba4a8add59e9c09558
kos-san <makocchya917@gmail.com>
2022-02-11 20:28:31 +0900

    navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '筋トレ記録アプリ'),
     );
+    // This is the theme of your application.
+    //
+    // Try running your application with "flutter run". You'll see the
+    // application has a blue toolbar. Then, without quitting the app, try
+    // changing the primarySwatch below to Colors.green and then invoke
+    // "hot reload" (press "r" in the console where you ran "flutter run",
+    // or simply save your changes to "hot reload" in a Flutter IDE).
+    // Notice that the counter didn't reset back to zero; the application
+    // is not restarted.
   }
 }
 
@@ -61,6 +61,27 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<BottomNavigationBarItem> bottomNavBarItems() {
+    return[
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'HOME',
+      ),
+      const BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: '記録',
+      ),
+      const BottomNavigationBarItem(
+          icon: Icon(Icons.health_and_safety_sharp),
+          label: '体重',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: '設定',
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -76,27 +97,14 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many tssidmes:',
+            ),
+            const Text(
+              'aa',
             ),
             Text(
               '$_counter',
@@ -104,12 +112,11 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNavBarItems(),
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
