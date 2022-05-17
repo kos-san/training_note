d6b85ee68b82e170667733de4928e342e6bc345c
sukepio <naoto927sukena@gmail.com>
2022-05-17 20:49:47 +0900

    エクササイズ詳細モーダル内の3点ドットモーダル表示

--- lib/view/parts/training_list.dart
+++ lib/view/parts/training_list.dart
@@ -9,6 +9,12 @@ class TrainingList extends StatefulWidget {
 
 class _TrainingListState extends State<TrainingList> {
   final items = List<String>.generate(20, (i) => "2021/04/$i");
+  var _modal_items = {'one': {'training_name': 'ベンチプレス'}, 'two': {'training_name': 'インクラインダンベルプレス'}};
+  var _numbers = <String, String>{
+    'one': 'イチ',
+    'two': 'ニ',
+    'three': 'サン',
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +49,7 @@ class _TrainingListState extends State<TrainingList> {
       body: Center(
         child: ListView.builder(
           itemCount: items.length,
-            itemBuilder: (context, index) {
+          itemBuilder: (context, index) {
              return InkWell(
                onTap: () async {
                  await showDialog(
@@ -52,16 +58,99 @@ class _TrainingListState extends State<TrainingList> {
                      return StatefulBuilder(
                          builder: (context, setState) {
                            return SimpleDialog(
-                             backgroundColor: Colors.black,
-                             shape: RoundedRectangleBorder(
-                               borderRadius: BorderRadius.all(Radius.circular(10.0))
-                             ),
+                               insetPadding: EdgeInsets.all(8),
+                               backgroundColor: Color(0XFFE7E0EC),
+                               shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(10.0))
+                               ),
                                contentPadding: EdgeInsets.all(0.0),
                                titlePadding: EdgeInsets.all(0.0),
                                title: Container(
-                                 height: 400,
-                                 child: Scaffold(
-                                   body: Center(child: Text('${items[index]}', style: TextStyle(fontSize: 40.0),)),
+                                 padding: EdgeInsets.all(10),
+                                 decoration: BoxDecoration(
+                                     color: Color(0XFFE7E0EC),
+                                     borderRadius: BorderRadius.circular(10),
+                                 ),
+                                 // height: 400,
+                                 child: Column(
+                                   children: [
+                                     Row(
+                                       crossAxisAlignment: CrossAxisAlignment.start,
+                                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                       children: [
+                                         IconButton(
+                                             padding: EdgeInsets.all(0.0),
+                                             constraints: BoxConstraints(),
+                                             onPressed: ()=> {
+                                               Navigator.of(context).pop()
+                                             },
+                                             icon: Icon(Icons.clear)
+                                         ),
+                                         Text('${items[index]}'),
+                                         IconButton(
+                                             onPressed: () async {
+                                               await showDialog(
+                                                   context: context,
+                                                   builder: (context) {
+                                                     return StatefulBuilder(
+                                                         builder: (context, setState) {
+                                                           return SimpleDialog(
+                                                             insetPadding: EdgeInsets.all(8),
+                                                             backgroundColor: Color(0XFFE7E0EC),
+                                                             shape: RoundedRectangleBorder(
+                                                                 borderRadius: BorderRadius.all(Radius.circular(10.0))
+                                                             ),
+                                                             contentPadding: EdgeInsets.all(0.0),
+                                                             titlePadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                                             title: Container(
+                                                               height: 100,
+                                                               width: 80,
+                                                               child: Column(
+                                                                 mainAxisAlignment: MainAxisAlignment.center,
+                                                                 crossAxisAlignment: CrossAxisAlignment.center,
+                                                                 children: [
+                                                                   Row(
+                                                                     children: [
+                                                                       Icon(Icons.mode_edit, color: Color(0XFF6750A4)),
+                                                                       Text('ワークアウトを編集')
+                                                                     ],
+                                                                   ),
+                                                                   SizedBox(height: 15),
+                                                                   Row(
+                                                                     children: [
+                                                                       Icon(Icons.clear, color: Colors.red),
+                                                                       Text('削除')
+                                                                     ],
+                                                                   ),
+                                                                 ],
+                                                               ),
+                                                             ),
+                                                           );
+                                                         },
+                                                     );
+                                                   },
+                                               );
+                                             },
+                                             icon: Icon(Icons.more_horiz)
+                                         ),
+                                       ],
+                                     ),
+                                     Container(
+                                       height: 300,
+                                       width: 320,
+                                       child: ListView.builder(
+                                           itemCount: _modal_items.length,
+                                           itemBuilder: (context, i){
+                                              var key = _modal_items.keys.elementAt(i);
+                                              return Card(
+                                                child: ListTile(
+                                                  title: Text('${_modal_items[key]!["training_name"]}, $index'),
+                                                ),
+                                              );
+                                           },
+                                       ),
+                                     ),
+                                   ],
                                  ),
                                )
                            );
